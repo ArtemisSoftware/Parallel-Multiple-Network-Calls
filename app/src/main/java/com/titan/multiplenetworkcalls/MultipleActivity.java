5cedbfd23524e8635a1f5d9c117ab7df142e4eb5
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-05-24 19:59:58 +0100

    MultipleApiCallsActivity

--- app/src/main/java/com/titan/multiplenetworkcalls/MultipleActivity.java
+++ app/src/main/java/com/titan/multiplenetworkcalls/MultipleActivity.java
@@ -6,9 +6,24 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.titan.multiplenetworkcalls.adapter.RecyclerViewAdapter;
+import com.titan.multiplenetworkcalls.api.CryptoCurrencyApi;
+import com.titan.multiplenetworkcalls.api.JsonplaceholderApi;
+import com.titan.multiplenetworkcalls.models.Crypto;
+import com.titan.multiplenetworkcalls.util.NetworkService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import timber.log.Timber;
 
 public abstract class MultipleActivity extends BaseActivity {
 
+    protected Observable<List<Crypto.Market>> btcObservable, ethObservable, errorObservable;
+    protected List<Observable<List<Crypto.Market>>> requests;
 
     RecyclerViewAdapter recyclerViewAdapter;
 
@@ -20,8 +35,145 @@ public abstract class MultipleActivity extends BaseActivity {
 
         initRecyclerView();
 
+        initObservables();
+
         initRequest();
     }
+
+
+
+    private void initObservables(){
+
+        CryptoCurrencyApi cryptoCurrencyApi = NetworkService.getCurrencyApi();
+        JsonplaceholderApi jsonplaceholderApi = NetworkService.getJsonplaceholderApi();
+
+
+        btcObservable = cryptoCurrencyApi.getCoinData("btc")
+                .map(new Function<Crypto, Observable<Crypto.Market>>() {
+
+                    @Override
+                    public Observable<Crypto.Market> apply(Crypto crypto) throws Exception {
+
+                        //Transformar a lista de markets numa observable source
+                        Timber.d("onMap");
+                        return Observable.fromIterable(crypto.ticker.markets);
+                    }
+                })
+
+                .flatMap(new Function<Observable<Crypto.Market>, ObservableSource<Crypto.Market>>() {
+                    @Override
+                    public ObservableSource<Crypto.Market> apply(Observable<Crypto.Market> marketObservable) throws Exception {
+
+                        Timber.d("onflatMap");
+                        return marketObservable;
+                    }
+                })
+                .filter(new Predicate<Crypto.Market>() {
+                    @Override
+                    public boolean test(Crypto.Market market) throws Exception {
+
+                        Timber.d("onfilter: " + market.toString());
+
+                        //filtrar cada market emitido pelo flatmap
+                        market.coinName = "btc";
+                        return true;
+                    }
+                })
+                .toList()
+                .toObservable();
+
+        ethObservable = cryptoCurrencyApi.getCoinData("eth")
+                .map(new Function<Crypto, Observable<Crypto.Market>>() {
+
+                    @Override
+                    public Observable<Crypto.Market> apply(Crypto crypto) throws Exception {
+
+                        //Transformar a lista de markets numa observable source
+                        Timber.d("onMap");
+                        return Observable.fromIterable(crypto.ticker.markets);
+                    }
+                })
+
+                .flatMap(new Function<Observable<Crypto.Market>, ObservableSource<Crypto.Market>>() {
+                    @Override
+                    public ObservableSource<Crypto.Market> apply(Observable<Crypto.Market> marketObservable) throws Exception {
+
+                        Timber.d("onflatMap");
+                        return marketObservable;
+                    }
+                })
+                .filter(new Predicate<Crypto.Market>() {
+                    @Override
+                    public boolean test(Crypto.Market market) throws Exception {
+
+                        Timber.d("onfilter: " + market.toString());
+
+                        //filtrar cada market emitido pelo flatmap
+                        market.coinName = "eth";
+                        return true;
+                    }
+                })
+                .toList()
+                .toObservable();
+
+        requests = new ArrayList<>();
+        requests.add(btcObservable);
+        requests.add(ethObservable);
+
+    }
+
+
+
+
+    protected void addErrorObservable(){
+
+        CryptoCurrencyApi cryptoCurrencyApi = NetworkService.getCurrencyApi();
+
+        errorObservable = cryptoCurrencyApi.getError()
+                .map(new Function<Crypto, Observable<Crypto.Market>>() {
+
+                    @Override
+                    public Observable<Crypto.Market> apply(Crypto crypto) throws Exception {
+
+                        //Transformar a lista de markets numa observable source
+                        Timber.d("onMap");
+                        return Observable.fromIterable(crypto.ticker.markets);
+                    }
+                })
+
+                .flatMap(new Function<Observable<Crypto.Market>, ObservableSource<Crypto.Market>>() {
+                    @Override
+                    public ObservableSource<Crypto.Market> apply(Observable<Crypto.Market> marketObservable) throws Exception {
+
+                        Timber.d("onflatMap");
+                        return marketObservable;
+                    }
+                })
+                .filter(new Predicate<Crypto.Market>() {
+                    @Override
+                    public boolean test(Crypto.Market market) throws Exception {
+
+                        Timber.d("onfilter: " + market.toString());
+
+                        //filtrar cada market emitido pelo flatmap
+                        market.coinName = "btc";
+                        return true;
+                    }
+                })
+                .toList()
+                .toObservable();
+
+
+        if(requests == null){
+            requests = new ArrayList<>();
+        }
+        requests.add(errorObservable);
+
+    }
+
+
+
+
 
 
 
--- app/src/main/java/com/titan/multiplenetworkcalls/MultipleApiCallsActivity.java
+++ app/src/main/java/com/titan/multiplenetworkcalls/MultipleApiCallsActivity.java
@@ -4,7 +4,100 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.titan.multiplenetworkcalls.api.JsonplaceholderApi;
+import com.titan.multiplenetworkcalls.models.Crypto;
+import com.titan.multiplenetworkcalls.models.Post;
+import com.titan.multiplenetworkcalls.util.NetworkService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function3;
+import io.reactivex.schedulers.Schedulers;
+
 public class MultipleApiCallsActivity extends MultipleActivity {
+
+    private Observable<List<List<Post>>> jsonObservable;
+
+    @Override
+    protected void initRequest() {
+
+        initJsonPlaceholderObservable();
+
+        zipMultipleDifferentApiCallsEndpoints();
+    }
+
+    private void initJsonPlaceholderObservable() {
+
+        JsonplaceholderApi jsonplaceholderApi = NetworkService.getJsonplaceholderApi();
+
+
+        jsonObservable = jsonplaceholderApi.getPosts()
+                /*
+                .map(new Function<List<Post>, List<Post>>() {
+                    @Override
+                    public List<Post> apply(List<Post> posts) throws Exception {
+                        return posts;
+                    }
+                })
+                .flatMap(new Function<List<Post>, ObservableSource<?>>() {
+                    @Override
+                    public ObservableSource<?> apply(List<Post> posts) throws Exception {
+                        return null;
+                    }
+                })
+                .filter(new Predicate<Object>() {
+                    @Override
+                    public boolean test(Object o) throws Exception {
+                        return false;
+                    }
+                })
+                */
+                .toList()
+                .toObservable();
+    }
+
+
+    private void zipMultipleDifferentApiCallsEndpoints(){
+
+        Observable.zip(ethObservable, jsonObservable, ethObservable, new Function3<List<Crypto.Market>, List<List<Post>>, List<Crypto.Market>, Object>() {
+            @Override
+            public List<String> apply(List<Crypto.Market> markets, List<List<Post>> lists, List<Crypto.Market> markets2) throws Exception {
+                List<String> list = new ArrayList();
+                list.add(markets.size() + " markets");
+                list.add(lists.size() + " posts");
+                list.add(markets2.size() + " markets2");
+                return list;
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Object>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
 
 
 }
