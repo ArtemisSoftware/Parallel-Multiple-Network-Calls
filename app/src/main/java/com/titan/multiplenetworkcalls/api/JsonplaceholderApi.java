3018c19132735d879bf7760b4a2b6a8f6387e9df
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-05-24 18:57:50 +0100

    JsonplaceholderApi

--- app/src/main/java/com/titan/multiplenetworkcalls/api/JsonplaceholderApi.java
+++ app/src/main/java/com/titan/multiplenetworkcalls/api/JsonplaceholderApi.java
@@ -1,4 +1,14 @@
 package com.titan.multiplenetworkcalls.api;
 
+import com.titan.multiplenetworkcalls.models.Post;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
 public interface JsonplaceholderApi {
+
+    @GET("posts")
+    Observable<List<Post>> getPosts();
 }
