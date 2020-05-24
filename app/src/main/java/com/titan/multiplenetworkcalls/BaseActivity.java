6abb607cee29ac03c0b2d31899b233d9367c0677
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-05-24 19:38:32 +0100

    BaseActivity

--- app/src/main/java/com/titan/multiplenetworkcalls/BaseActivity.java
+++ app/src/main/java/com/titan/multiplenetworkcalls/BaseActivity.java
@@ -7,23 +7,34 @@ import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class BaseActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+public abstract class BaseActivity extends AppCompatActivity {
 
     public ProgressBar mProgressBar;
 
     @Override
     public void setContentView(int layoutResID) {
 
-        //ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
-        //FrameLayout frameLayout = constraintLayout.findViewById(R.id.activity_content);
-        //mProgressBar = constraintLayout.findViewById(R.id.progress_bar);
+        ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
+        FrameLayout frameLayout = constraintLayout.findViewById(R.id.activity_content);
+        mProgressBar = constraintLayout.findViewById(R.id.progress_bar);
 
-        //getLayoutInflater().inflate(layoutResID, frameLayout, true);
+        getLayoutInflater().inflate(layoutResID, frameLayout, true);
         super.setContentView(null/*constraintLayout*/);
     }
 
     public void showProgressBar(boolean visible) {
 
         mProgressBar.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    public void showSnack(){
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
+
+        Snackbar snackbar = Snackbar
+                .make(constraintLayout, "www.journaldev.com", Snackbar.LENGTH_LONG);
+        snackbar.show();
     }
 }
