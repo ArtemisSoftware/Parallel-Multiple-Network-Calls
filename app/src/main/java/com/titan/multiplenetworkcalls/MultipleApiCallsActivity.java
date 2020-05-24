af2dcc409dc8b26758b6268d300144e6e350dcb9
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-05-24 19:34:16 +0100

    Layout activity_base

---
+++ app/src/main/java/com/titan/multiplenetworkcalls/MultipleApiCallsActivity.java
@@ -0,0 +1,14 @@
+package com.titan.multiplenetworkcalls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MultipleApiCallsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiple_api_calls);
+    }
+}
--- app/src/main/java/com/titan/multiplenetworkcalls/MultipleCallsActivity.java
+++ app/src/main/java/com/titan/multiplenetworkcalls/MultipleCallsActivity.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 
 import com.titan.multiplenetworkcalls.adapter.RecyclerViewAdapter;
 import com.titan.multiplenetworkcalls.api.CryptoCurrencyApi;
+import com.titan.multiplenetworkcalls.api.JsonplaceholderApi;
 import com.titan.multiplenetworkcalls.models.Crypto;
+import com.titan.multiplenetworkcalls.models.Post;
 import com.titan.multiplenetworkcalls.util.NetworkService;
 
 import java.util.ArrayList;
@@ -20,6 +22,7 @@ import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
+import io.reactivex.functions.Function3;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
@@ -29,6 +32,7 @@ public class MultipleCallsActivity extends AppCompatActivity {
     RecyclerViewAdapter recyclerViewAdapter;
 
     private Observable<List<Crypto.Market>> btcObservable, ethObservable, errorObservable;
+    private Observable<List<List<Post>>> jsonObservable;
     private List<Observable<List<Crypto.Market>>> requests;
 
     @Override
@@ -41,12 +45,16 @@ public class MultipleCallsActivity extends AppCompatActivity {
         //initErrorObservables();
 
         //callMultipleEndpoint();
-        zipMultipleCallsEndpoint();
+        //zipMultipleCallsEndpoint();
+
+        zipMultipleDifferentApiCallsEndpoints();
     }
 
     private void initObservables(){
 
         CryptoCurrencyApi cryptoCurrencyApi = NetworkService.getCurrencyApi();
+        JsonplaceholderApi jsonplaceholderApi = NetworkService.getJsonplaceholderApi();
+
 
         btcObservable = cryptoCurrencyApi.getCoinData("btc")
                 .map(new Function<Crypto, Observable<Crypto.Market>>() {
@@ -113,6 +121,31 @@ public class MultipleCallsActivity extends AppCompatActivity {
                         return true;
                     }
                 })
+                .toList()
+                .toObservable();
+
+
+        jsonObservable = jsonplaceholderApi.getPosts()
+                /*
+                .map(new Function<List<Post>, List<Post>>() {
+                    @Override
+                    public List<Post> apply(List<Post> posts) throws Exception {
+                        return posts;
+                    }
+                })
+                .flatMap(new Function<List<Post>, ObservableSource<?>>() {
+                    @Override
+                    public ObservableSource<?> apply(List<Post> posts) throws Exception {
+                        return null;
+                    }
+                })
+                .filter(new Predicate<Object>() {
+                    @Override
+                    public boolean test(Object o) throws Exception {
+                        return false;
+                    }
+                })
+                */
                 .toList()
                 .toObservable();
 
@@ -304,6 +337,44 @@ public class MultipleCallsActivity extends AppCompatActivity {
                     }
                 });
         */
+    }
+
+
+    private void zipMultipleDifferentApiCallsEndpoints(){
+
+        Observable.zip(ethObservable, jsonObservable, ethObservable, new Function3<List<Crypto.Market>, List<List<Post>>, List<Crypto.Market>, Object>() {
+            @Override
+            public List<String> apply(List<Crypto.Market> markets, List<List<Post>> lists, List<Crypto.Market> markets2) throws Exception {
+                List<String> list = new ArrayList();
+                list.add(markets.size() + " markets");
+                list.add(lists.size() + " posts");
+                list.add(markets2.size() + " markets2");
+                return list;
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Object>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 
 
